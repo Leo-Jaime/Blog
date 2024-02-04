ed25f0a892dc41fa531b9b1004bbaba7607f7370
leo <leojaime253@gmail.com>
2024-02-04 17:10:00 -0300

    Alterado para enviar email para um destinario fixo.

--- enviar_email.py
+++ enviar_email.py
@@ -3,10 +3,10 @@ import os
 
 USER = os.environ.get('USER_EMAIL')
 SENHA = os.environ.get('USER_SENHA')
-
-def enviar_email(email, mensagem):
+EMAIL_DESTINO = os.environ.get('EMAIL_DETINO')
+def enviar_email(mensagem):
     conexao = smtplib.SMTP("smtp.gmail.com")
     conexao.starttls()
     conexao.login(user=USER, password=SENHA)
-    conexao.sendmail(USER, email, mensagem)
+    conexao.sendmail(USER, EMAIL_DESTINO, mensagem)
     conexao.quit()
